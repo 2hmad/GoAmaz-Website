faa76631dba200c5c523548ee8e7e89def578ac3
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-11-13 17:23:57 +0200

    Add icon

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="icon" href="{{ asset('favicon.ico') }}">
 <link rel="stylesheet" href="{{ asset('sass/index.css') }}" />
 @if (LaravelLocalization::getCurrentLocale() == 'ar')
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap" rel="stylesheet">
