143b2847ca188cc1c351bc74d9e9c1be94d701ad
Ahmed Mohamed Ibrahim <eng.ahmedmohamed.2002@gmail.com>
2021-11-15 02:47:05 +0200

    Update admin panel

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -6,7 +6,8 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset('admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ $data->name }}</a>
